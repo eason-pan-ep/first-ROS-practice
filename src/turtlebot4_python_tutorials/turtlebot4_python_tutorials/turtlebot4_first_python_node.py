5eeb850b61d376ab4c311e3832e87ed936ea8aca
eason-pan-ep <eason.pan@proton.me>
2025-01-27 20:58:31 -0800

    first commit

---
+++ src/turtlebot4_python_tutorials/turtlebot4_python_tutorials/turtlebot4_first_python_node.py
@@ -0,0 +1,13 @@
+from irobot_create_msgs.msg import InterfaceButtons, LightringLeds
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+
+
+def main():
+    print('Hi from turtlebot4_python_tutorials.')
+
+
+if __name__ == '__main__':
+    main()
