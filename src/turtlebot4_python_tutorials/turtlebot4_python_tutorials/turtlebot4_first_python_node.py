8bb711a1cf0a99461c54befaface754079f31617
eason-pan-ep <eason.pan@proton.me>
2025-01-27 21:15:53 -0800

    created a node

--- src/turtlebot4_python_tutorials/turtlebot4_python_tutorials/turtlebot4_first_python_node.py
+++ src/turtlebot4_python_tutorials/turtlebot4_python_tutorials/turtlebot4_first_python_node.py
@@ -4,9 +4,17 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 
+class TurtleBot4FirstNode(Node):
+    def __init__(self):
+        super().__init__('turtlebot4_first_python_node')
 
-def main():
-    print('Hi from turtlebot4_python_tutorials.')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TurtleBot4FirstNode()
+    rclpy.spin(node=node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
